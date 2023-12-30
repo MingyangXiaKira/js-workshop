877ca9743926b0dcadfa259ac0b8f274c5f6071c
Mingyang <141198127+MingyangXiaKira@users.noreply.github.com>
2023-12-30 15:17:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import Cards from "./components/Cards";
 import WorkDetail1 from "./Pages/WorkDetails/1/WorkDetail1";
 import WorkDetail2 from "./Pages/WorkDetails/2/WorkDetail2";
 import Home from "./Pages/Home";
+import WorkDetail3 from "./Pages/WorkDetails/3/WorkDetail3";
 // ...导入其他 WorkDetail 组件
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="https://MingyangXiaKira.github.io/js-workshop">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/card/1" element={<WorkDetail1 />} />
         <Route path="/card/2" element={<WorkDetail2 />} />
+        <Route path="/card/3" element={<WorkDetail3 />} />
       </Routes>
     </BrowserRouter>
   );
